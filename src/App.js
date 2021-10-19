ef685708dd149102f4a8cfaaafde853e7131eaae
Nouman Khan <noumanijaz.dev@outlook.com>
2021-10-19 11:28:48 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import News from "./components/News";
+import Header from "./components/Header";
+
+export default class App extends Component {
+	render() {
+    return (
+      <div>
+       <Header/>
+       <News/>
+    </div>
+    )
+	}
+}
